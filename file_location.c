8e01482d03e4c01b6fe30c774320bde71c5ef464
shukufabayramzada <shukufabayramzada@gmail.com>
2024-04-30 13:28:08 +0000

    add new function slash_checker to check that filename starts with slash or not

--- file_location.c
+++ file_location.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * slash_checker - check the file nmae that it begins with slash or not
+ * @str: str to be checked
+ * Return: 0 in success or 1 in failure
+ **/
+int slash_checker(const char *str)
+{
+	if (str != NULL || str[0] == '/')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
  * get_file_loc - get the executable path of the file
  * @path: full path variable
  * @file_name: executable file
