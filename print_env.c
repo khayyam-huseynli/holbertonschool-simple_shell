20709ec35f876aad5b37e6a80412a018090903e1
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-05-01 23:34:02 +0400

    creating print_env.c

---
+++ print_env.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *print_env - prints all environment variables
+ */
+void print_env(void)
+{
+  char **env = environ;
+  while (*env != NULL)
+  {
+    printf("%s\n", *env);
+    env++;
+  }
+}
