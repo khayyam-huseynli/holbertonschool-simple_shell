5e36a3b596cf40a095d8b1b1afe26f42674a6631
khayyam <khayyam.huseynli@gmail.com>
2024-05-01 18:59:55 +0400

    handled PATH

--- execmd.c
+++ execmd.c
@@ -3,17 +3,24 @@
 /**
  * execute_cmd - executes a command
  * @cmd: the command to execute
- *
+ * @argv: the arguments of command
  * Return: 0 on success, 1 on failure
  */
 
 int execute_cmd(char *cmd, char **argv)
 {
 	int status;
+	char *fullpath = get_file_path(cmd);
+
+	if (fullpath == NULL)
+	{
+		fprintf(stderr, "%s: command not found\n", cmd);
+		return (-1);
+	}
 
 	if (fork() == 0)
 	{
-		execve(cmd, argv, NULL);
+		execve(fullpath, argv, NULL);
 		perror(cmd);
 		exit(EXIT_FAILURE);
 	}
@@ -22,5 +29,6 @@ int execute_cmd(char *cmd, char **argv)
 		wait(&status);
 	}
 
+	free(fullpath);
 	return (0);
 }
