bbd563eeccaa0fa850f3646d563c8660fff52fd7
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-02 14:12:03 +0000

    tried to add update

--- execmd.c
+++ execmd.c
@@ -6,18 +6,16 @@
  * @argv: the arguments of command
  * Return: 0 on success, 1 on failure
  */
-
 int execute_cmd(char *cmd, char **argv, char *name)
 {
 	int status;
 	char *fullpath = get_file_path(cmd);
-
+	
 	if (fullpath == NULL || *fullpath == '\0')
 	{
 		fprintf(stderr, "%s: 1: %s: not found\n", name, cmd);
 		return (127);
 	}
-
 	if (fork() == 0)
 	{
 		execve(fullpath, argv, NULL);
@@ -28,7 +26,6 @@ int execute_cmd(char *cmd, char **argv, char *name)
 	{
 		wait(&status);
 	}
-
 	free(fullpath);
 	return (0);
 }
