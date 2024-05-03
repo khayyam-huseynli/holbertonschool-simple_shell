17504e4bde5b84b30959310a3931d55b5ee36c27
shukufabayramzada <shukufabayramzada@gmail.com>
2024-05-03 13:37:07 +0000

    update for tokenize function

--- process_line.c
+++ process_line.c
@@ -8,26 +8,35 @@
  * Return: 0 on success, 1 on failure
  */
 
-int process_line(char *line, char *name)
+int process_line(char *line, char *args[])
 {
-	char *cmds[MAX_CMD_LEN];
-	char *argv[MAX_ARGV_LEN];
-	char *cmd;
-	int num_cmds = 0, i, err_stat = 0;
-
-	/** Tokenize the line into commands and store them in cmds */
-	cmd = strtok(line, " \n");
-	while (cmd != NULL)
+	/*
+	 * char *cmds[MAX_CMD_LEN];
+	 * char *argv[MAX_ARGV_LEN];
+	*/
+	int num_tokens = 0;
+	char *token;
+	   
+	/* Tokenize the line into commands and store them in cmds */
+	token = strtok(line, " \n");
+	while (token)
 	{
-		if (strlen(cmd) > 0 && cmd[0] != ' ')
+		args[num_tokens] = token;
+		/*
+		 * if (strlen(cmd) > 0 && cmd[0] != ' ')
 		{
 			cmds[num_cmds] = strdup(cmd);
 			num_cmds++;
 		}
-		cmd = strtok(NULL, " \n");
+		*/
+		token = strtok(NULL, " \n");
+		num_tokens++;
 	}
+	args[num_tokens] = NULL;
+	return (num_tokens);
+}
 
-	 if (num_cmds > 0 && strcmp(cmds[0], "env") == 0)
+/**	 if (num_cmds > 0 && strcmp(cmds[0], "env") == 0)
    	 {
         	print_env();
 		for (i = 0; i < num_cmds; i++)
@@ -40,24 +49,32 @@ int process_line(char *line, char *name)
 	 {
 		return (0);
 	 }
+	 **/
 	
 	/** Prepare the arguments for the command */
-	for (i = 0; i < num_cmds; i++)
+	/**
+	 * for (i = 0; i < num_cmds; i++)
 	{
 		argv[i] = cmds[i];
 	}
 	argv[i] = NULL;
+	**/
 	
 	/** Execute the command with arguments */
-	if (num_cmds > 0)
+	/**
+	 * if (num_cmds > 0)
 	{
 		err_stat = execute_cmd(cmds[0], argv, name);
 	}
+	**/
 	/** Free the allocated memory */
-	for (i = 0; i < num_cmds; i++)
+	/**
+	 * for (i = 0; i < num_cmds; i++)
 	{
 		free(cmds[i]);
 	}
 	return (err_stat);
 
-}
+}**/
+
+
