e9a49bc4c518c4650993ffdfb9b6a104c12944bc
khayyam <khayyam.huseynli@gmail.com>
2024-05-05 20:31:54 +0400

    bug fixed

--- file_location.c
+++ file_location.c
@@ -73,8 +73,8 @@ char *get_file_path(char *file_name)
 	char *path = _getenv("PATH");
 	char *full_path;
 
-	if (path == NULL && (!slash_checker(file_name) &&
-			access(file_name, X_OK) != 0))
+	if (!path && !slash_checker(file_name) &&
+			access(file_name, X_OK) != 0)
 		return (NULL);
 
 	if (slash_checker(file_name) &&
